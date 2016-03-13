e67daca6c240c7354e7479d656d42c134895dd35
faielomente <frlomente@up.edu.ph>
2016-03-13 17:18:37 +0800

    for submittion

--- ByteOrdering.py
+++ ByteOrdering.py
@@ -1,10 +1,6 @@
-import numpy
 
 
 class ByteOrdering:
-    big_matrix = []
-    little_matrix = []
-
     # Reads the file and return the content
     def read(self):
         temp = open("input.in", 'r').read().split('\n')
@@ -13,24 +9,27 @@ class ByteOrdering:
         return temp
 
     # Print the data
-    def print_data(self, data):
+    @staticmethod
+    def print_data(data):
         data = list(data)
         string = ""
         for letter in data:
             string += letter + "   "
         print(string)
 
-    def to_big(self, data):
+    def store_string(self, data, tobigendian):
         data = self.add_zeros(data)
-        self.big_matrix = self.to_4byte(data)
-        for index in self.big_matrix:
-            self.print_data(index)
+        data_list = self.to_4byte(data, False)
 
-    def to_little(self, data):
-        data = self.add_zeros(data)
-        self.little_matrix = self.to_4byte(data)
-        for index in self.little_matrix:
-            self.print_data(index[::-1])
+        for index in data_list:
+            if tobigendian is True:
+                self.print_data(index)
+            else:
+                self.print_data(index[::-1])
+
+    def store_integer(self, data):
+        data_list = self.to_4byte(data, True)
+        self.print_data(data_list)
 
     def add_zeros(self, data):
         ctr = 0
@@ -39,25 +38,43 @@ class ByteOrdering:
             ctr += 1
         return data
 
-    def to_4byte(self, data):
+    def to_4byte(self, data, isinteger):
         start = 0
         end = 0
         data_array = []
-        while end < len(data):
-            end = start + 4
-            byte_string = data[start:end:]
-            data_array.append(byte_string)
-            start = end
-        print(data_array)
+        if isinteger is False:
+            while end < len(data):
+                end = start + 4
+                byte_string = data[start:end:]
+                data_array.append(byte_string)
+                start = end
+        else:
+            n = 8
+            data_array = [data[index: index + n] for index in range(0, len(data), n)]
+            self.parse(data_array)
         return data_array
+
+    def parse(self, binary_array):
+        for i in range(len(binary_array)):
+            binary_array[i] = str(int(binary_array[i], 2))
 
 
 b = ByteOrdering()
 text_data = b.read()
 print("BIG ENDIAN FORMAT")
 for line in text_data:
-    b.to_big(line)
+    if line.isdigit() is False:
+        b.store_string(line, True)
+    else:
+        digit = int(line)
+        binary_str = "{0:032b}".format(digit)
+        b.store_integer(binary_str)
 
 print("LITTLE ENDIAN FORMAT")
 for line in text_data:
-    b.to_little(line)
+    if line.isdigit() is False:
+        b.store_string(line, False)
+    else:
+        digit = int(line)
+        binary_str = "{0:032b}".format(digit)
+        b.store_integer(binary_str)
